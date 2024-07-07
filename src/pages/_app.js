83af4503a140eb02c7dd945640b585faa9b1e381
Cenagaurav77 <sinhagaurav7788@gmail.com>
2024-07-07 13:45:48 -0400

    Modified files to integrate Google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,43 @@
-import '@/styles/globals.css'
+// pages/_app.js
+
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+
+const handleRouteChange = (url) => {
+  window.gtag("config", "G-JXG0RYGPPC", {
+    page_path: url,
+  });
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JXG0RYGPPC', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JXG0RYGPPC"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
